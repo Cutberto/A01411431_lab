3946a709d49fa3e46a76809298f07a90e18e8e40
cutberto <cutbertoan@gmail.com>
2021-02-13 22:17:37 -0600

    solución de problema 4 en problema4.html

---
+++ lab4/js/prob4.js
@@ -0,0 +1,59 @@
+window.alert("hola");
+
+function promedios( matriz ) {
+    var ncolumnas = matriz[0].length;
+    var nfilas = matriz.length;
+    
+    let sumatemp = 0;
+    let promediotemp = 0;
+    
+    var resp = [];
+
+    for (var i = 0; i < nfilas; i++ ){
+
+        sumatemp = 0;
+        for (let j = 0; j < ncolumnas; j++){
+             sumatemp += matriz[i][j];
+            //sumatemp++;
+        }
+        promediotemp = sumatemp / ncolumnas;
+        resp.push(promediotemp);
+    }
+    return resp;
+    
+  
+    
+    
+   // return [ncolumnas,nfilas];
+}
+
+//driver code 
+let proms = [];
+proms.push([10,8,7,8]);
+proms.push([10,10,10,10]);
+proms.push([10,10,5,5]);
+proms.push([10,8,7,8]);
+
+let respuesta = promedios(proms);
+let salida = document.getElementById("output");
+let temptxt = "El promedio de cada fila de la matriz <br> ";
+    var ncolumnas = proms[0].length;
+    var nfilas = proms.length;
+for (let i = 0; i < nfilas; i++ ){
+        temptxt+= "[";
+        for (let j = 0; j < ncolumnas; j++){
+            temptxt += proms[i][j];
+            temptxt += ", ";
+        }
+        temptxt += "]<br>";
+    }
+temptxt += "es de: <br> ";
+
+for (let i = 0; i < respuesta.length ; i++) {
+    temptxt += "[";
+    temptxt += respuesta[i];
+    temptxt += "] <br>";
+    
+}
+
+salida.innerHTML = temptxt;
