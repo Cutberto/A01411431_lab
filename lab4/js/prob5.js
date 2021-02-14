34d283fd0a17734181462daefe2ec251787eaa1c
cutberto <cutbertoan@gmail.com>
2021-02-14 14:37:14 -0600

    problema 5 resuelto en problema5.html

--- lab4/js/prob5.js
+++ lab4/js/prob5.js
@@ -1,17 +1,17 @@
-window.alert("hola");
 
-function inverso( matriz ) {
-    
-    return resp;
-    
+function inverso( numero ) {
+ inverted = "";    
+ for (let i = numero.length-1; i >= 0; i--) {
+     inverted += numero[i];
+ }
   
-    
+    return inverted;
     
 }
 
 //driver code 
 
-
-let temptxt = "";
-
+let num = window.prompt("Inserta el numero a invertir");
+let temptxt = "El resultado es: " + inverso(num);
+let salida = document.getElementById("output");
 salida.innerHTML = temptxt;
