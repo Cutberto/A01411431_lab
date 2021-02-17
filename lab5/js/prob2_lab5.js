99077f3b7b8e52d9e15cdcefcba948f1c3deed0b
cutberto <cutbertoan@gmail.com>
2021-02-17 10:17:54 -0600

    ejercicio 2 termniado lab5

--- lab5/js/prob2_lab5.js
+++ lab5/js/prob2_lab5.js
@@ -1,36 +1,105 @@
-
-function validarPassword(pass){
-    if (pass.length < 8) {
-        return ("La longitud minima es de 8 caracteres.");
-    }
-    /*
-    let countMayus = 0;
-    let countMinus = 0;
-    let countNumber = 0;
-    let countSpecial = 0;
+function actualizarCarrito(cantMario, cantPlantas, cantClub){
+    let temptxt = "";
+    let salida = document.getElementById("output");
+    let calcPrecio = 0;
     
-    for (let i=0; i < pass.length; i++) {
+    if (cantMario > 4) {
+        window.alert("Ya alcanzaste el máximo de elementos mario");
+        return 0;
+    }
+    else if (cantMario > 0){
+        temptxt += "<li> Mario Kart: " + cantMario + " en el carrito </li>";
+        calcPrecio = 1;
+    }
+    if (cantPlantas > 4) {
+        window.alert("Ya alcanzaste el máximo de elementos");
+        return 0;
+    }
+    else if (cantPlantas > 0){
+        temptxt += "<li> Plantas vs Zombies: " + cantPlantas  + " en el carrito </li>";
+        calcPrecio=1;
+    }
+    if (cantClub > 4) {
+        window.alert("Ya alcanzaste el máximo de elementos");
+        return 0;
+    }
+    else if (cantClub > 0){
+        temptxt += "<li> Club pinguino: " + cantClub + " en el carrito </li>";
+        calcPrecio=1;
+    }
+        
+    
+    if (calcPrecio == 1){
+        var subtotal = (parseInt(cantMario)*200);
+        subtotal += parseInt(cantPlantas)*100;
+        subtotal += parseInt(cantClub)*300;
+        var iva = parseFloat(subtotal) * 0.16;
+        var total = subtotal + iva;
+        temptxt+= " <hr><h2>Subtotal: </h2>  $"+subtotal;
+        temptxt+= " <h2>iva: </h2> $"+iva;
+        temptxt+= " <h2>Total: </h2>  $"+total;
+        
         
     }
-    */
     
-
-    var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-    if (strongRegex.test(pass)){
-        return ("Esta contraseña es fuerte");
-    }
-    else {
-        return ("La contraseña es válida, pero débil.");
-    }
-    
-    return " ";
+    salida.innerHTML = temptxt; 
 }
 
+var btnMario = document.getElementById("btnMario");
+var btnPlantas = document.getElementById("btnPlantas");
+var btnClub = document.getElementById("btnClub");
 
-let password = document.getElementById("password");
-let salida = document.getElementById("output");
+var cantMario = 0;
+var cantPlantas = 0;
+var cantClub = 0;
+var tmp =0;
+btnMario.onclick=()=> {
+    if ( cantMario < 3 ){
+        tmp= parseInt(window.prompt("¿Cuántos quieres agregar al carrito? El máximo es 3"));
+        
+        if (tmp <= 3) {
+            cantMario+=tmp;
+            actualizarCarrito(cantMario, cantPlantas, cantClub);
+            }
+        else {
+            window.alert("Si agregas " +tmp + " elementos, excedes el límite de productos :( ");
+        }
+        }
+    else {
+        window.alert("Ya alcanzaste el limite de Mario Kart");
+    }
+}
 
-password.onkeyup = () => {
-    let valor_password = password.value;
-    salida.innerHTML = validarPassword(valor_password);
+btnPlantas.onclick=()=> {
+    if ( cantPlantas < 3 ){
+            tmp= parseInt(window.prompt("¿Cuántos quieres agregar al carrito? El máximo es 3"));
+        
+        if (tmp <= 3) {
+            cantPlantas+=tmp;
+            actualizarCarrito(cantMario, cantPlantas, cantClub);   
+            }
+        else {
+            window.alert("Si agregas " +tmp + " elementos, excedes el límite de productos :( ");
+        }
+    }
+    else {
+        window.alert("Ya alcanzaste el limite de Plantas vs zombies");
+    }
+}
+
+btnClub.onclick=()=> {
+    if ( cantClub < 3 ){
+            tmp= parseInt(window.prompt("¿Cuántos quieres agregar al carrito? El máximo es 3"));
+        
+        if (tmp <= 3) {
+            cantClub+=tmp;
+            actualizarCarrito(cantMario, cantPlantas, cantClub);
+            }
+        else {
+            window.alert("Si agregas " +tmp + " elementos, excedes el límite de productos :( ");
+        }    
+    }
+    else {
+        window.alert("Ya alcanzaste el limite de Club Penguin");
+    }
 }
