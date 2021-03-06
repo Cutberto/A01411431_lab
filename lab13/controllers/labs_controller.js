3e744aa5d5e7b49c56cc03598209412f86c8c7ba
cutberto <cutbertoan@gmail.com>
2021-03-06 17:04:14 -0600

    lab 12 convertido a MVC

---
+++ lab13/controllers/labs_controller.js
@@ -0,0 +1,24 @@
+
+
+
+exports.lab3 = (request, response, next) => {
+    response.render('lab3');
+
+};
+
+
+exports.lab4 = (request, response, next) => {
+    response.render('lab4_preguntas');
+
+};
+
+exports.lab6 = (request, response, next) => {
+    response.render('lab6');
+
+};
+
+exports.lab5 = (request, response, next) => {
+    response.render('preguntas_lab5');
+
+};
+
--- lab13/routes/laboratorios.js
+++ lab13/routes/laboratorios.js
@@ -4,31 +4,14 @@ const router = express.Router();
 
 const path = require('path');
 
+const labController = require('../controllers/labs_controller');
 
 
-router.get('/laboratorio3', (request, response, next) => {
-    //response.sendFile(path.join(__dirname, '..' ,  'views', 'lab3.html'));
-    response.render('lab3');
-});
-
-router.get('/laboratorio4', (request, response, next) => {
-    //response.sendFile(path.join(__dirname, '..' ,  'views', 'lab4_preguntas.html'));
-    response.render('lab4_preguntas');
-});
-
-
-router.get('/laboratorio6', (request, response, next) => {
-    //response.sendFile(path.join(__dirname, '..' ,  'views', 'lab6.html'));
-    response.render('lab6');
-});
-
-router.get('/laboratorio5', (request, response, next) => {
-    //response.sendFile(path.join(__dirname, '..' ,  'views', 'preguntas_lab5.html'));
-    response.render('preguntas_lab5');
-});
-
-
-
+router.get('/laboratorio3' ,  labController.lab3  );
+router.get('/laboratorio4' ,  labController.lab4  );
+router.get('/laboratorio5' ,  labController.lab5  );
+router.get('/laboratorio6' ,  labController.lab6  );
+ 
 
 
 
