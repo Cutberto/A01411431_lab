73ced7cd8e797a768bcca0aeea9c7b2f97ad826a
cutberto <cutbertoan@gmail.com>
2021-03-11 23:28:43 -0600

    lab 16 terminado

--- lab16/insert_data.sql
+++ lab16/insert_data.sql
@@ -35,4 +35,39 @@ BULK INSERT a1411431.a1411431.[Entregan]
          FIELDTERMINATOR = ',',
          ROWTERMINATOR = '\n'
       )
-	  
+	
+	
+ALTER TABLE Materiales add constraint llaveMateriales PRIMARY KEY (Clave)
+ ALTER TABLE Proveedores add constraint llaveProveedores PRIMARY KEY (RFC)
+ ALTER TABLE Proyectos add constraint llaveProyectos PRIMARY KEY (Numero)
+ ALTER TABLE Entregan add constraint llaveEntregan PRIMARY KEY (Clave, RFC, Numero, Fecha)
+
+
+INSERT INTO Materiales values(1000, 'xxx', 1000)
+
+ sp_helpconstraint Materiales
+ sp_helpconstraint Proveedores
+ sp_helpconstraint Proyectos
+ sp_helpconstraint Entregan
+
+ALTER TABLE Materiales drop constraint llaveMateriales
+
+SELECT * FROM Materiales
+SELECT * FROM Proveedores
+SELECT * FROM Proyectos
+SELECT * FROM Entregan
+
+INSERT INTO entregan values (0, 'xxx', 0, '1-jan-02', 0) ;
+DELETE FROM Entregan WHERE Clave = 0
+
+ALTER TABLE entregan add constraint cfentreganclave foreign key (clave) references materiales(clave);
+ALTER TABLE entregan add constraint cfentreganRFC foreign key (RFC) references Proveedores(RFC);
+ALTER TABLE entregan add constraint cfentregannumero foreign key (Numero) references Proyectos(Numero);
+
+sp_helpconstraint Materiales
+
+INSERT INTO entregan values (1000, 'AAAA800101', 5000, GETDATE(), 0);
+   
+Delete from Entregan where Cantidad = 0
+   
+ALTER TABLE entregan add constraint cantidad check (cantidad > 0) ;
